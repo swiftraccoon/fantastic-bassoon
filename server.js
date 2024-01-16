cd9137a81c99efc82cc909946ff536ccb26825ee
swiftraccoon <swiftraccoon-gh@wnc.icu>
2024-01-15 20:01:33 -0500

    Added dotenv and express middlewares for better logging and rate limiting
    Introduced dotenv for environment variable management, facilitating the configuration process. Enhanced request logging capabilities with morgan and implemented rate limiting to protect against abuse with express-rate-limit. These additions improve the application's maintainability, security, and operability.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const rateLimit = require('express-rate-limit');
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+require('dotenv').config();
 
 // Logging Middleware
 app.use(morgan('combined'));
